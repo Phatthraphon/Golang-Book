c885bdce193764307a0bf512f9cdbe93815c6358
Phatthraphon <33712516+Phatthraphon@users.noreply.github.com>
2018-02-08 09:42:50 +0700

    edit enter for chapter3-3

--- Chapter3-3/main.go
+++ Chapter3-3/main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println(">>>>>>>>>>> Floating point <<<<<<<<<<<")
 	third := 1.0/3.0
 	fmt.Printf("third = %v\n",third)
-	fmt.Printf("third + third + third := %v\n",third+third+third)	
+	fmt.Printf("third + third + third := %v\n",third+third+third"\n")	
 	fmt.Println(">>>>>>>>>>> Comparing Floating point <<<<<<<<<<<")
 	fmt.Println("0.1 + 0.2 == 0.3 is", 0.1+0.2 ==0.3)
 	num := 0.1
