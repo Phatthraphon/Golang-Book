aae8e53d9be22d949cbc4e34daa671da76f5a159
Phatthraphon <phatthraphon.ch@tnis.com>
2018-02-08 10:37:23 +0700

    Add Chapter 4-2

--- Chapter4-2/main.go
+++ Chapter4-2/main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Enter a number : ")
-	var input float64
-	fmt.Scanf("%f", &input)
-	output := input * 2
-	fmt.Println(output)
+	//fmt.Print("Enter a number : ")
+	//var input float64
+	//fmt.Scanf("%f", &input)
+	//output1 := input * 2
+	//fmt.Println(output1)
+
+	fmt.Print("Enter Fahrenheit : ")
+	var F float64
+	fmt.Scanf("%f", &F)
+	Celsius := (F-32)*5/9
+	fmt.Printf("Celsius : %.2f", Celsius)
 }
